ada5841b23b16bb4bfc21e54afe11b92210293ac
clem9281 <clem9281@gmail.com>
2019-03-20 16:33:43 -0700

    fixed a problem with put, got delete working

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -4,46 +4,21 @@ const userDb = require("../data/helpers/userDb");
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(async (req, res) => {
-    // get all the posts
-    try {
-      const posts = await postDb.get();
-      res.status(200).json(posts);
-    } catch (error) {
-      res.status(500).json({ error: "Sorry, we couldn't find the posts" });
-    }
-  })
-  // the update needs a post id, not a user id. the id endpoint below is for the user id. i wasn't sure if i should make a new endpoint for post id, or just send the post id in the body. i ended up sending it in the body, but idk what has the better user experience
-  .put(async (req, res) => {
-    try {
-      const { text, id } = req.body;
-      if (!text || !id) {
-        return res.status(400).json({
-          error: "You must include text content and post id for your post."
-        });
-      }
-      const updated = await postDb.update(id, {
-        text: text
-      });
-      if (updated === 0) {
-        return res
-          .status(404)
-          .json({ error: "The post you are trying to update does not exist" });
-      }
-      const posts = await postDb.get();
-      res.status(201).json(posts);
-    } catch (error) {
-      res.status(500).json({ error: "Sorry, we can't update that post" });
-    }
-  });
+router.route("/").get(async (req, res) => {
+  // get all the posts
+  try {
+    const posts = await postDb.get();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: "Sorry, we couldn't find the posts" });
+  }
+});
 
 router
-  .route("/:id")
+  .route("/:userId")
   .get(async (req, res) => {
     try {
-      const userPosts = await userDb.getUserPosts(req.params.id);
+      const userPosts = await userDb.getUserPosts(req.params.userId);
       res.status(200).json(userPosts);
     } catch (error) {
       res
@@ -60,7 +35,7 @@ router
           .json({ error: "You must include text content for your post." });
       }
       //   if the user_id passed in doesn't match an existing user id return 404 --- it occured to me after I wrote this it might not be necessary?
-      const user = await userDb.getById(req.params.id);
+      const user = await userDb.getById(req.params.userId);
       if (!user) {
         return res.status(404).json({
           error: "You are an invalid user, we could not add that post"
@@ -68,7 +43,7 @@ router
       }
       const newPost = await postDb.insert({
         text: req.body.text,
-        user_id: req.params.id
+        user_id: req.params.userId
       });
       const posts = await postDb.get();
       res.status(201).json(posts);
@@ -79,4 +54,42 @@ router
     }
   });
 
+router
+  .route("/:postId")
+  .put(async (req, res) => {
+    try {
+      const { text } = req.body;
+      if (!text) {
+        return res.status(400).json({
+          error: "You must include text content for your post."
+        });
+      }
+      const updated = await postDb.update(req.params.postId, req.body);
+      if (updated === 0) {
+        return res
+          .status(404)
+          .json({ error: "The post you are trying to update does not exist" });
+      }
+      const posts = await postDb.get();
+      res.status(201).json(posts);
+    } catch (error) {
+      res.status(500).json({ error: "Sorry, we can't update that post" });
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      const deleted = await postDb.remove(req.params.postId);
+      if (deleted === 0) {
+        return res
+          .status(404)
+          .json({ error: "There is not post to delete at that post id" });
+      }
+      const posts = await postDb.get();
+      res.status(200).json(posts);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Sorry we were unable to delete that post" });
+    }
+  });
 module.exports = router;
