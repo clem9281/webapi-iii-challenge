1f20304f8a996a66a2610455f3ffd09218a7d261
clem9281 <clem9281@gmail.com>
2019-03-20 15:20:30 -0700

    added a get posts endpoint

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,14 +1,48 @@
 const express = require("express");
 const postDb = require("../data/helpers/postDb");
+const userDb = require("../data/helpers/userDb");
 
 const router = express.Router();
 
-router.route("/").get(async (req, res) => {
+router
+  .route("/")
+  .get(async (req, res) => {
+    // get all the posts
+    try {
+      const posts = await postDb.get();
+      res.status(200).json(posts);
+    } catch (error) {
+      res.status(500).json({ error: "Sorry, we couldn't find the posts" });
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      console.log(req.body);
+      //   if (!req.body.text) {
+      //     return res
+      //       .status(400)
+      //       .json({ error: "You must include text content for your post." });
+      //   }
+      const newPost = await postDb.insert(req.body);
+      console.log(newPost);
+      const posts = await postDb.get();
+      console.log(posts);
+      res.status(201).json(posts);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Sorry, we couldn't add a post right now" });
+    }
+  });
+
+router.route("/:id").get(async (req, res) => {
   try {
-    const posts = await postDb.get();
-    res.status(200).json(posts);
+    const userPosts = await userDb.getUserPosts(req.params.id);
+    res.status(200).json(userPosts);
   } catch (error) {
-    res.status(500).json({ error: "Sorry, we couldn't find the posts" });
+    res
+      .status(500)
+      .json({ error: "Sorry, we can't find any posts for that user" });
   }
 });
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -29,10 +29,38 @@ router
         .json({ error: "Sorry, we couldn't add a user at this time" });
     }
   });
-//   .put(capitalizeName, async (req, res) => {
-//     try {
-//       const newUser = await userDb.
-//     } catch (error) {}
-//   });
+
+router
+  .route("/:id")
+  // update user
+  .put(capitalizeName, async (req, res) => {
+    try {
+      const newUser = await userDb.update(req.params.id, req.body);
+      if (newUser === 0) {
+        return res
+          .status(404)
+          .json({ error: "There is no user with that id number" });
+      }
+      const users = await userDb.get();
+      res.status(201).json(users);
+    } catch (error) {
+      res.status(500).json({ error: "Sorry, we couldn't update that user" });
+    }
+  })
+  // delete user
+  .delete(async (req, res) => {
+    try {
+      const deleted = await userDb.remove(req.params.id);
+      if (deleted === 0) {
+        return res
+          .status(404)
+          .json({ error: "There is no user with that id number" });
+      }
+      const users = await userDb.get();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ error: "Sorry, we couldn't delete that user" });
+    }
+  });
 
 module.exports = router;
